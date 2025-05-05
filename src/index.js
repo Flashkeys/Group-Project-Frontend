97e7000e17d6e4e5976f96e168a2afb97bcc698c
Lundet <hannes.j.lundin@gmail.com>
2025-05-05 14:11:34 +0200

    Added files

--- src/index.js
+++ src/index.js
@@ -3,14 +3,26 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from './jsx/Home.jsx';
 import ShowAllPosts from './jsx/ShowAllPosts.jsx';
+import Profile from './jsx/Profile.jsx';
+import LikedPosts from './jsx/LikedPosts.jsx';
+import EditProfile from './jsx/EditProfile.jsx';
+import Login from './jsx/Login.jsx';
+import Register from './jsx/Register.jsx';
+import Logout from './jsx/Logout.jsx';
 
 const Index = () => {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/showAllPosts" element={<ShowAllPosts/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/showAllPosts" element={<ShowAllPosts />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/liked" element={<LikedPosts />} />
+        <Route path="/edit" element={<EditProfile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/logout" element={<Logout />} />
       </Routes>
     </BrowserRouter>
   )
--- src/jsx/Header.jsx
+++ src/jsx/Header.jsx
@@ -2,19 +2,34 @@ import React from "react";
 import '../css/Components.css';
 
 const Header = () => {
-
+  const isLoggedIn = true; // Replace with actual logic
+  const userProfileImage = "https://via.placeholder.com/40"; // Replace with user's actual profile image URL
 
   return (
     <div>
-      <div>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/">Register</a></li>
-        </ul>
-      </div>
+      <ul>
+        <li><a href="/">Home</a></li>
+        {isLoggedIn ? (
+          <>
+            <li>
+              <a href="/profile">
+                <img src={userProfileImage} alt="Profile" />
+              </a>
+            </li>
+            <li><link href="/liked">Liked Posts</link></li>
+            <li><link href="/messages">Messages</link></li>
+            <li><link href="/edit">Edit Profile</link></li>
+            <li><link href="/logout">Logout</link></li>
+          </>
+        ) : (
+          <>
+            <li><link href="/login">Login</link></li>
+            <li><link href="/register">Register</link></li>
+          </>
+        )}
+      </ul>
     </div>
   );
-
 }
 
 export default Header;
