e2e19ce8cad19702a79d4a465efd32233a031049
Lundet <hannes.j.lundin@gmail.com>
2025-05-14 15:01:46 +0200

    Sidebar and Header in likedpost

--- src/jsx/LikedPosts.jsx
+++ src/jsx/LikedPosts.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import FilterAllPosts from "./FilterAllPosts";
+import SideBar from "./SideBar";
+import Header from "./Header";
 
 const LikedPosts = () => {
   const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
   const likedPosts = existingUsers.flatMap((user, userIndex) =>
-    user.posts
-      .filter(post => post.likedBy.includes(currentUser.username))
+    (user.posts || [])
+      .filter(post => post.likedBy?.includes(currentUser.username))
       .map((post, postIndex) => ({
         ...post,
         username: user.username,
@@ -17,7 +19,17 @@ const LikedPosts = () => {
       }))
   );
 
-  return <FilterAllPosts posts={likedPosts} currentUser={currentUser} showEditDelete={false} />;
+  return (
+    <>
+      <Header />
+      <div className="main-content">
+        <SideBar />
+        <div className="post-content">
+          <FilterAllPosts posts={likedPosts} currentUser={currentUser} showEditDelete={false} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LikedPosts;
